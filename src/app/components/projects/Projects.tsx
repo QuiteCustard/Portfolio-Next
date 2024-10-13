c07cf5ef86f5588afc9036b2ac32620fbf802ece
QuiteCustard <56020602+QuiteCustard@users.noreply.github.com>
2024-10-13 19:03:15 +0100

    Add IOT and descriptions to icons

--- src/app/components/projects/Projects.tsx
+++ src/app/components/projects/Projects.tsx
@@ -9,15 +9,20 @@ export default function Projects() {
             <h2 className={`text-5xl font-semibold`}>Projects</h2>
             {projects.map(({ name, description, url, icons, image, github }, index) => (
                 <article key={index} className="flex flex-col gap-4 border-b-2 pb-8 justify-between lg:flex-row lg:gap-8">
-                    <Image src={image} alt={name} width={215} height={215} className="rounded" />
+                    <Image src={image} alt={name} width={265} height={265} className="rounded" />
                     <div className="flex flex-col gap-6">
                         <h3 className="text-2xl">{name}</h3>
                         <p>{description}</p>
-                        <Link href={url} className="bg-secondary p-2 text-black rounded-xl w-fit transition-colors hover:bg-primary hover:text-white" target="_blank">Visit {name}</Link>
-                        {github && <Link href={github} className="bg-tertiary p-2 text-black rounded-xl w-fit" target="_blank">View code</Link>}
+                        <div className="flex gap-4">
+                            <Link href={url} className="bg-secondary p-2 text-black rounded-xl w-fit transition-colors hover:bg-primary hover:text-white" target="_blank">Visit {name}</Link>
+                            {github && <Link href={github} className="bg-primary p-2 text-white rounded-xl w-fit transition-colors hover:bg-secondary hover:text-black" target="_blank">View code</Link>}
+                        </div>
                         <ul className="flex gap-4">
-                            {icons.map(({title, icon}, index) => (
-                                <li key={index}><FontAwesomeIcon icon={icon} height={35} width={35} className="text-white" role="img" aria-label={title} /></li>
+                            {icons.map(({ title, icon }, index) => (
+                                <li key={index} className="flex flex-col gap-2 items-center">
+                                    <FontAwesomeIcon icon={icon} height={35} width={35} className="text-white" role="img" aria-label={title} />
+                                    <small>{title}</small>
+                                </li>
                             ))}
                         </ul>
                     </div>
--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,7 +1,28 @@
-import { faCss3Alt, faHtml5, faReact, faWordpress } from "@fortawesome/free-brands-svg-icons";
+import { faCss3Alt, faHtml5, faJs, faPhp, faReact, faWordpress } from "@fortawesome/free-brands-svg-icons";
 import type { Project } from "../types";
 
 export const projects: Project[] = [
+    {
+        name: "Pokédex",
+        description: "A Pokédex built using NextJS and PokeAPI. I wanted to design and make it look like the generation 9 in game Pokédex.",
+        url: "https://pokedex-coral-nu.vercel.app/",
+        image: "/pokedex.webp",
+        github: "https://github.com/QuiteCustard/pokedexT3",
+        icons: [
+            {
+                title: "React (NextJS)",
+                icon: faReact
+            },
+            {
+                title: "CSS3",
+                icon: faCss3Alt
+            },
+            {
+                title: "TypeScript",
+                icon: faJs
+            }
+        ],
+    },
     {
         name: "CRB1",
         description: "CRB is a chartered account website that I built using WordPress. WordPress was requested by the client as they wished to have a blogging platform.",
@@ -23,19 +44,23 @@ export const projects: Project[] = [
         ],
     },
     {
-        name: "Pokédex",
-        description: "A Pokédex built using NextJS and PokeAPI. I wanted to design and make it look like the generation 9 in game Pokédex.",
-        url: "https://pokedex-coral-nu.vercel.app/",
-        image: "/pokedex.webp",
-        github: "",
+        name: "IOT",
+        description: "A project I built in University that was a part of my Internet of Things module. It was used to monitor and control a Raspberry Pi that could control air conditioning, heating, and opening/closing windows. The username and password can be found on the repo.",
+        url: "https://ws269058-iot.remote.ac/",
+        image: "/iot.webp",
+        github: "https://github.com/QuiteCustard/iot",
         icons: [
             {
-                title: "React (NextJS)",
-                icon: faReact
+                title: "PHP",
+                icon: faPhp
             },
             {
                 title: "CSS3",
                 icon: faCss3Alt
+            },
+            {
+                title: "JavaScript",
+                icon: faJs
             }
         ],
     }
